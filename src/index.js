fa0e8a63e38976708f3fa558f4f500d52ec9ffa0
Morten Möller <git@mortenm.de>
2021-04-27 13:04:22 +0200

    Add 4 buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 console.clear()
 const box = document.querySelector('.box')
-const buttonAdd = document.querySelector('.button-add')
-const buttonRemove = document.querySelector('.button-remove')
-const buttonToggle = document.querySelector('.button-toggle')
+const buttonRotate = document.querySelector('.button-rotate')
+const buttonBackground = document.querySelector('.button-background')
+const buttonRadius = document.querySelector('.button-radius')
+const buttonShake = document.querySelector('.button-shake')
 
-buttonAdd.addEventListener('click', () => {
-  box.classList.add('blue')
+buttonRotate.addEventListener('click', () => {
+  box.classList.toggle('rotation-back')
+  box.classList.toggle('rotation')
 })
 
-buttonRemove.addEventListener('click', () => {
-  box.classList.remove('blue')
+buttonBackground.addEventListener('click', () => {
+  box.classList.toggle('background')
 })
 
-buttonToggle.addEventListener('click', () => {
-  box.classList.toggle('blue')
+buttonRadius.addEventListener('click', () => {
+  box.classList.toggle('radius')
+})
+
+buttonShake.addEventListener('click', () => {
+  box.classList.add('shake')
+  setTimeout(() => {
+    box.classList.remove('shake')
+    console.log('remove class shake')
+  }, 2500)
 })
